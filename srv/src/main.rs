bf9a5fd31d0002ddde82a063d90bdab09a8175ab
Quin-Darcy <quintondarcy@csus.edu>
2023-04-22 18:22:58 -0700

    Refactored server code to parse messages according to GreetingProtocol
    - Add TransistionRule struct
    
    - Add transition_rules function to return vector of custom TransitionRules
    
    - Add apply_rules method to apply rules against recived header and payload
    
    - Add extract_header_and_payload function

--- srv/src/main.rs
+++ srv/src/main.rs
@@ -15,36 +15,69 @@ enum ServerState {
     Goodbye,
 }
 
-fn process_request(buffer: &Vec<u8>, state: &ServerState) -> (String, ServerState) {
-    let message: &str = str::from_utf8(buffer)
-        .expect("Could not write to buffer as string");
+struct TransitionRule {
+    header: u32,
+    payload: &'static str,
+    from_state: ServerState,
+    response: &'static str,
+    to_state: ServerState,
+}
 
-    println!("CLIENT: {:?}", message);
+fn transition_rules() -> Vec<TransitionRule> {
+    vec![
+        TransitionRule {
+            header: 0x48454c4f,
+            payload: "Hello!\n",
+            from_state: ServerState::Initial,
+            response: "200;OK;Hello, client!\n",
+            to_state: ServerState::Greeted,
+        },
+        TransitionRule {
+            header: 0x5449455f,
+            payload: "What time is it?\n",
+            from_state: ServerState::Greeted,
+            response: "201;OK;",
+            to_state: ServerState::TimeRequested,
+        },
+        TransitionRule {
+            header: 0x4259455f,
+            payload: "Goodbye!\n",
+            from_state: ServerState::TimeRequested,
+            response: "202;OK;Goodbye, client!\n",
+            to_state: ServerState::Goodbye,
+        },
+    ]
+}
 
-    let response: String;
-    let new_state: ServerState;
-
-    match (message, state) {
-        ("Hello!\n", ServerState::Initial) => {
-            response = String::from("200 OK: HELLO - Hello, client!\n");
-            new_state = ServerState::Greeted;
-        }
-        ("What time is it?\n", ServerState::Greeted) => {
-            let time: String = format!("{}\n", Local::now().format("%H:%M:%S"));
-            let mut time_response: String = String::from("201 OK: TIME - ");
-            time_response.push_str(time.as_str());
-            response = time_response;
-            new_state = ServerState::TimeRequested;
-        }
-        ("Goodbye!\n", ServerState::TimeRequested) => {
-            response = String::from("202 OK: GOODBYE - Goodbye, client!\n");
-            new_state = ServerState::Goodbye;
-        }
-        _ => {
-            response = String::from("400 ERROR: INVALID_REQUEST - Invalid request received\n");
-            new_state = state.clone();
+fn apply_rules(header: u32, payload: &str, state: &ServerState, rules: &[TransitionRule]) -> (String, ServerState) {
+    for rule in rules {
+        if header == rule.header && payload == rule.payload && state == &rule.from_state {
+            let mut response = String::from(rule.response);
+            if rule.response.ends_with(";") {
+                let time = format!("{}\n", Local::now().format("%H:%M:%S"));
+                response.push_str(time.as_str());
+            }
+            return (response, rule.to_state.clone());
         }
     }
+
+    // If no rules match, return an error response
+    (String::from("400;ERROR;Invalid request received\n"), state.clone())
+}
+
+fn extract_header_and_payload(buffer: &[u8]) -> (u32, String) {
+    let header = u32::from_be_bytes([buffer[0], buffer[1], buffer[2], buffer[3]]);
+    let payload_length = u32::from_be_bytes([buffer[4], buffer[5], buffer[6], buffer[7]]) as usize;
+    let payload = String::from_utf8(buffer[8..(8 + payload_length)].to_vec()).unwrap();
+
+    (header, payload)
+}
+
+fn process_request(header: u32, payload: &str, state: &ServerState) -> (String, ServerState) {
+    println!("CLIENT: {:?}", payload);
+
+    let rules = transition_rules();
+    let (response, new_state) = apply_rules(header, payload, state, &rules);
 
     println!("SERVER: {:?}", response.as_str());
     return (response, new_state);
@@ -57,12 +90,16 @@ fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
         let mut reader: BufReader<&TcpStream> = BufReader::new(&stream);
         let mut buffer: Vec<u8> = Vec::new();
         let response: String;
-        let bytes_read: usize  = reader.read_until(b'\n', &mut buffer)
-                                    .expect("Could not read into buffer");
+        let bytes_read: usize = reader.read_until(b'\n', &mut buffer).expect("Could not read into buffer");
+
         if bytes_read != 0 {
-            let (response, new_state) = process_request(&buffer, &server_state);
+            // Extract header and payload from the message
+            let (header, payload) = extract_header_and_payload(&buffer);
+
+            let (response, new_state) = process_request(header, &payload, &server_state);
             server_state = new_state;
             stream.write(response.as_bytes())?;
+
             if server_state == ServerState::Goodbye {
                 stream.shutdown(Shutdown::Both).expect("shutdown call failed");
             }
