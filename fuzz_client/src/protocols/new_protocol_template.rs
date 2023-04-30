e919071d12b2c3fbace54cc9fb7b81d9afd1b7a3
Quin-Darcy <quintondarcy@csus.edu>
2023-04-30 15:54:52 -0700

    Added template for new protocol implementations

---
+++ fuzz_client/src/protocols/new_protocol_template.rs
@@ -0,0 +1,109 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+
+use std::hash::Hash;
+use std::cmp::PartialEq;
+use std::fmt::Debug;
+
+use crate::Message;
+use crate::Response;
+
+
+pub struct YourProtocol {
+    // Add any required fields and states for your protocol here.
+}
+
+impl Protocol for YourProtocol {
+    type MessageType = YourProtocolMessageType;
+    type MessageSectionsKey = YourProtocolMessageSectionsKey;
+    type MessageSectionsValue = YourProtocolMessageSectionsValue;
+
+    type ServerState = YourProtocolServerState;
+
+    fn random_message(&self) -> Message<Self> {
+        // Generate a random message for your protocol and return it.
+        todo!();
+    }
+
+    fn build_message(&self, message_bytes: &[u8]) -> Message<Self> {
+        // Build a message for your protocol from the given byte slice and return it.
+        todo!();
+    }
+
+    fn mutate_message(&self, message: &Message<Self>) -> Message<Self> {
+        // Mutate the given message for your protocol and return the mutated message.
+        todo!();
+    }
+
+    fn crossover_messages(&self, message1: &Message<Self>, message2: &Message<Self>) -> (Message<Self>, Message<Self>) {
+        // Perform crossover on the given messages for your protocol and return the resulting pair of messages.
+        todo!();
+    }
+
+    fn parse_response(&self, response: &Response) -> Self::ServerState {
+        // Parse the given response and return the corresponding server state for your protocol.
+        todo!();
+    }
+}
+
+// Define your protocol-specific types below.
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct YourProtocolMessageType {
+    // Add any required fields for your protocol message type here.
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Debug)]
+pub struct YourProtocolMessageSectionsKey {
+    // Add any required fields for your protocol message sections key here.
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct YourProtocolMessageSectionsValue {
+    // Add any required fields for your protocol message sections value here.
+}
+
+#[derive(Clone, Eq, PartialEq, Hash, Debug)]
+pub struct YourProtocolServerState {
+    // Add any required fields for your protocol server state here.
+}
+
+impl Default for YourProtocolMessageType {
+    fn default() -> Self {
+        // Default YourProtocolMessageType
+        todo!();
+    }
+}
+
+impl Default for YourProtocolMessageSectionsKey {
+    fn default() -> Self {
+        // Default YourProtocolMessageSectionsKey, like Self::Header or Self::Payload
+        todo!();
+    }
+}
+
+impl Default for YourProtocolMessageSectionsValue {
+    fn default() -> Self {
+        // Default YourProtocolMessageSectionsValue
+        todo!();
+    }
+}
+
+#[derive(Clone, Eq, PartialEq, Hash)]
+pub struct YourProtocolServerState {
+    // Fields which make up structure of YourProtocol's server responses
+}
+
+impl Debug for YourProtocolServerState {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        /*
+        write!(
+            f,
+            "{} - {}: {}",
+            self.field1, self.field2, self.field3, etc
+        )
+        */
+        todo!();
+    }
+}
