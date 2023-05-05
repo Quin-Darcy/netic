bae554c171b354136bc90556c5e85e6db32e95b0
Quin-Darcy <quintondarcy@csus.edu>
2023-05-04 22:33:18 -0700

    Added time modulation in each secret server state

--- srv/src/state_machine.rs
+++ srv/src/state_machine.rs
@@ -2,6 +2,8 @@
 #![allow(unused_variables)]
 
 use std::collections::HashMap;
+use std::thread;
+use std::time::Duration;
 
 use crate::Message;
 use crate::MessageType;
@@ -95,34 +97,44 @@ impl StateMachine {
     pub fn handle_message(&mut self, message: &Message, transition_rules: &StateTransitionRules) -> Response {
         let (response, new_state) = Response::new(message, &self.current_state, transition_rules);
 
+        let mut sleep_duration: Duration = Duration::from_secs(0);
+
         if !message.parsing_results.is_empty() {
             if self.current_state == ServerState::Questioned && message.parsing_results[0].clone() == ServerError::InvalidPayloadLength {
                 println!("---------------SECRET1---------------");
                 self.current_state = ServerState::Secret1;
+                sleep_duration = Duration::from_secs(1);
+
             }
 
             if self.current_state == ServerState::Secret1 && message.parsing_results[0].clone() == ServerError::UnrecognizedPayload {
                 println!("---------------SECRET2---------------");
                 self.current_state = ServerState::Secret2;
+                sleep_duration = Duration::from_secs(2);
             }
 
             if self.current_state == ServerState::Secret2 && message.parsing_results[0].clone() == ServerError::HeaderMismatch {
                 println!("---------------SECRET3---------------");
                 self.current_state = ServerState::Secret3;
+                sleep_duration = Duration::from_secs(3);
             }
 
             if self.current_state == ServerState::Secret3 && message.parsing_results[0].clone() == ServerError::InvalidPayloadLength {
                 println!("---------------SECRET4---------------");
                 self.current_state = ServerState::Secret4;
+                sleep_duration = Duration::from_secs(4);
             }
 
             if self.current_state == ServerState::Secret4 && message.parsing_results[0].clone() == ServerError::NonUTF8Sequence {
                 println!("---------------SECRET4---------------");
                 self.current_state = ServerState::Secret5;
+                sleep_duration = Duration::from_secs(5);
             }
         } else {
             self.current_state = new_state;
         }
+
+        thread::sleep(sleep_duration);
         response
     }
 }
